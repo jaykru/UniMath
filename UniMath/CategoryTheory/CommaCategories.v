77e5e9fa55378b1045ca06a807a2bd466b7eb34f
jay kruer <j@pop-os.localdomain>
2021-12-21 16:29:01 -0800

    wip proof of cartesian closure for special commas

--- UniMath/CategoryTheory/CommaCategories.v
+++ UniMath/CategoryTheory/CommaCategories.v
@@ -332,3 +332,319 @@ Proof.
 Defined.
 
 End general_comma_categories.
+
+Require Import UniMath.CategoryTheory.exponentials.
+Require Import UniMath.CategoryTheory.limits.terminal.
+Require Import UniMath.CategoryTheory.limits.binproducts.
+Require Import UniMath.CategoryTheory.Core.Isos.
+Section cc_comma.
+  (* Context (C D : category). *)
+  (* Context (F : C ⟶ D). *)
+
+  Definition cc_structure (C: category) : UU := ∑
+                                                                (chosen_terminal : Terminal C)
+                                                                (choice_prods: BinProducts C),
+                                                                Exponentials choice_prods.
+
+  Definition cc_category := ∑ (C: category), cc_structure C.
+  Definition cccat_to_cat : cc_category -> category := pr1.
+
+  Coercion cccat_to_cat: cc_category >-> category.
+  Definition cccat_to_ccs : forall {C : cc_category}, cc_structure C := pr2.
+  Coercion cccat_to_ccs: cc_category >-> cc_structure.
+
+  Definition has_terminal {C} (c: cc_structure C) : Terminal C := pr1 c.
+  Definition has_prods {C} (c : cc_structure C) : BinProducts C := pr1 (pr2 c).
+  Definition has_exponentials {C} (c : cc_structure C) : Exponentials _ := (pr2 (pr2 c)).
+  Definition term_ob {C} (c : cc_structure C) : C := (pr1 (pr1 c)).
+
+  Definition chosen_prod {C : cc_category} (x y : C): C.
+      pose proof (has_prods C) as prods_C.
+      unfold BinProducts, BinProduct, isBinProduct in prods_C.
+      specialize (prods_C x y).
+      destruct prods_C as [[P [pi1 pi2]] _].
+      exact P.
+Defined.
+
+
+    Definition chosen_exp {C : cc_category} (x y : C): C := (pr1 (has_exponentials C x) y). (* defined by the right-adjoint to the functor _ × x, namely _^ˣ *)
+
+  Notation " y ^ x " := (chosen_exp x y). (* exponentiation by x *)
+
+  Definition is_cc_functor {C: cc_category} {D: cc_category}
+                        (F : C ⟶ D) : UU
+    := (iso (term_ob D) (F (term_ob C))) ×
+
+         (∏ (x y: C), iso (F (chosen_prod x y))
+                                     (chosen_prod (F x) (F y))) ×
+
+         (∏ (x y : C), iso (F (chosen_exp x y)) (chosen_exp (F x) (F y))).
+
+Definition cc_functor (C D : cc_category) := ∑ (F : C ⟶ D), is_cc_functor F.
+
+  Definition term_iso {C D : cc_category} {F: C ⟶ D}
+             (c: is_cc_functor F)  := pr1 c.
+
+  Definition prod_iso {C D : cc_category} {F: C ⟶ D}
+             (c: is_cc_functor F) := pr1(pr2 c).
+
+  Definition exp_iso {C D : cc_category} {F: C ⟶ D}
+             (c: is_cc_functor F) := pr2 (pr2 c).
+
+Lemma iso_inv_before_iso : ∏ {C : category} {a b : C} (I: iso a b), iso_inv_from_iso I · I = identity b.
+  intros.
+  erewrite iso_inv_to_right.
+  reflexivity.
+  simpl.
+  rewrite id_left.
+  reflexivity.
+Defined.
+
+Lemma iso_to_term_is_term: ∏ {C : category} (t_C : Terminal C) (t' : C), iso (TerminalObject t_C) t' -> isTerminal C t'.
+  Proof.
+    intros.
+    unfold isTerminal.
+    intros.
+    unfold iscontr.
+    unshelve econstructor.
+    exact (TerminalArrow t_C a · X).
+    intros.
+    assert (t · iso_inv_from_iso X = TerminalArrow t_C a).
+    eapply TerminalArrowUnique.
+    rewrite <-X0.
+    rewrite <-assoc.
+    rewrite (@iso_inv_before_iso C t_C t' X).
+    rewrite id_right.
+    reflexivity.
+Defined.
+
+Lemma iso_to_term_is_term': ∏ {C : category} (t_C : Terminal C) (t' : C), iso (TerminalObject t_C) t' -> Terminal C.
+  intros.
+  exact (make_Terminal _  (iso_to_term_is_term t_C t' X)).
+Defined.
+
+Lemma cc_func_of_term_is_term : forall {C D : cc_category} {F : C ⟶ D},
+    is_cc_functor F -> isTerminal _ (F (term_ob C)).
+  intros C D F F_is_cc.
+  eapply iso_to_term_is_term.
+  apply F_is_cc.
+Defined.
+
+  Theorem gluing_cc_along_cc_is_cc :
+    ∏ (C : cc_category) (R : cc_category) (T : C ⟶ R),
+           is_cc_functor T ->
+           cc_structure (comma_category T (functor_identity R)).
+  Proof.
+    intros.
+    unfold cc_structure.
+    constructor.
+    { (* terminal object in gl *)
+      (* ( termob(D) , termob_iso? , termob(C)) *)
+      unfold Terminal.
+      unshelve econstructor.
+      unshelve econstructor.
+
+      { exact (term_ob R ,, term_ob C). }
+      { simpl.
+        exact (term_iso X).
+      }
+      { unfold isTerminal.
+        intros.
+        unfold iscontr.
+        unshelve econstructor.
+        { (* canonical morphism into termob *)
+          unshelve econstructor.
+          split.
+          {
+            simpl.
+            exact (TerminalArrow (has_terminal R) (pr1 (pr1 a))) .
+          }
+          {
+            simpl.
+            exact (TerminalArrow (has_terminal C) (pr2 (pr1 a))) .
+          }
+          {
+            simpl.
+            destruct a as [[a_R a_C] int_a].
+            simpl.
+            simpl in *.
+
+            unshelve epose TerminalArrowUnique.
+            { exact R. }
+
+
+
+            epose (make_Terminal _ (cc_func_of_term_is_term X)).
+            specialize (p t a_R).
+            transitivity (TerminalArrow t a_R); auto.
+          }
+        }
+        {
+          intro.
+          destruct t as [[syn sem] comma_condition].
+          simpl in comma_condition.
+          destruct a as [[a_R a_C] int_a].
+          simpl in *.
+          eassert (syn = TerminalArrow _ _).
+          eapply TerminalArrowUnique.
+          eassert (sem = TerminalArrow _ _).
+          eapply TerminalArrowUnique.
+          unfold has_terminal.
+
+          generalize comma_condition.
+          rewrite X0, X1.
+          intros.
+          clear comma_condition.
+          rename comma_condition0 into comma_condition.
+          Fail eapply (total2_paths2 (a1 := (TerminalArrow _ a_R,, TerminalArrow _ a_C)) (a2 := (TerminalArrow _ a_R,, TerminalArrow _ a_C)) (b1 := comma_condition) (b2 := match
+                                                                                                                                                                             TerminalArrowUnique
+                                                                                                                                                                               (make_Terminal (T (term_ob C)) (cc_func_of_term_is_term X)) a_R
+                                                                                                                                                                               (TerminalArrow
+                                                                                                                                                                                  (make_Terminal (T (term_ob C)) (cc_func_of_term_is_term X)) a_R) in
+                                                                                                                                                                             (_ = y) return (int_a · # T (TerminalArrow _ a_C) = y)
+                                                                                                                                                                           with
+                                                                                                                                                                           | idpath _ =>
+                                                                                                                                                                               TerminalArrowUnique
+                                                                                                                                                                                 (make_Terminal (T (term_ob C)) (cc_func_of_term_is_term X)) a_R
+                                                                                                                                                                                 (int_a · # T (TerminalArrow _ a_C))
+                                                                                                                                                                           end)).
+
+          Fail eapply (total2_paths2 (a1 := {| pr1 := syn; pr2 := sem |}) (a2:={|
+                                                                                 pr1 := TerminalArrow (has_terminal R) a_R;
+                                                                                 pr2 := TerminalArrow (has_terminal C) a_C |}) (b1 := comma_condition) (b2 := match
+                                                                                                                                                               TerminalArrowUnique
+                                                                                                                                                                 (make_Terminal (T (term_ob C)) (cc_func_of_term_is_term X)) a_R
+                                                                                                                                                                 (TerminalArrow
+                                                                                                                                                                    (make_Terminal (T (term_ob C)) (cc_func_of_term_is_term X)) a_R) in
+                                                                                                                                                               (_ = y) return (int_a · # T (TerminalArrow (has_terminal C) a_C) = y)
+                                                                                                                                                             with
+                                                                                                                                                             | idpath _ =>
+                                                                                                                                                                 TerminalArrowUnique
+                                                                                                                                                                   (make_Terminal (T (term_ob C)) (cc_func_of_term_is_term X)) a_R
+                                                                                                                                                                   (int_a · # T (TerminalArrow (has_terminal C) a_C))
+                                                                                                                                                             end) _ _).
+          eapply PartA.pair_path2 ; simpl.
+          unfold transportf.
+          unfold constr1.
+          simpl.
+          admit.
+        }
+      }
+    }
+    unshelve econstructor.
+    {
+
+      (* binary products *)
+      unfold BinProducts.
+      unfold BinProduct.
+      intros.
+      unshelve econstructor.
+      {
+        unshelve econstructor.
+
+        { (* product object *)
+          destruct c as [[c_R c_C] int_c].
+          destruct d as [[d_R d_C] int_d].
+
+          pose (has_prods R) as prods_R.
+          unfold BinProducts, BinProduct, isBinProduct in prods_R.
+          specialize (prods_R c_R d_R).
+          destruct prods_R as [[P' [pi1' pi2']] _].
+
+          unshelve econstructor.
+          { (* pair *)
+            constructor; [exact  P' | exact (chosen_prod c_C d_C)].
+          }
+          { (* interpretation *)
+            simpl in *.
+            pose (prods_R := has_prods R (T c_C) (T d_C)).
+            unfold BinProducts, BinProduct, isBinProduct in prods_R.
+
+            pose (P := pr1 (pr1 prods_R)).
+            pose (prod_UP := pr2 prods_R).
+
+            destruct X as [_ [prod_iso _]].
+            eapply (postcompose ((iso_inv_from_iso (prod_iso _ _)))).
+
+            specialize (prod_UP P' (pi1' · int_c) (pi2' · int_d)).
+
+            destruct prod_UP as [[pair _] _].
+            unfold chosen_prod.
+            unfold has_prods.
+            eapply pair.
+          }
+        }
+
+        {
+          (* product projection *)
+           destruct c as [[c_R c_C] int_c];
+           destruct d as [[d_R d_C] int_d];
+
+           cbv beta;
+
+
+          constructor.
+          {
+            (* first projection *)
+            unshelve econstructor.
+            constructor;
+            simpl in *.
+            {
+              pose (prods_R := has_prods R c_R d_R).
+              unfold BinProducts, BinProduct, isBinProduct in prods_R.
+
+              pose (P := pr1 (pr1 prods_R)).
+              pose (pi1 := pr1 (pr2 (pr1 prods_R))).
+              pose (pi2 := pr2 (pr2 (pr1 prods_R))).
+              simpl in pi1, pi2.
+
+              pose (ump_prod := pr2 prods_R).
+              simpl in ump_prod.
+              exact pi1.
+            }
+            {
+              pose (prods_C := has_prods C c_C d_C).
+              unfold BinProducts, BinProduct, isBinProduct in prods_C.
+
+              pose (P := pr1 (pr1 prods_C)).
+              pose (pi1 := pr1 (pr2 (pr1 prods_C))).
+              pose (pi2 := pr2 (pr2 (pr1 prods_C))).
+              simpl in pi1, pi2.
+
+              pose (ump_prod := pr2 prods_C).
+              simpl in ump_prod.
+              exact pi1.
+            }
+            {
+             simpl in *.
+             pose (pi1R := pr1 (pr2 (pr1 (has_prods R c_R d_R)))).
+             pose (pi1C := pr1 (pr2 (pr1 (has_prods C c_C d_C)))).
+             simpl in pi1R,pi1C.
+             unfold postcompose.
+             erewrite <-(_ : pi1R = pr1 (pr2 (pr1 (has_prods R c_R d_R)))).
+             erewrite <-(_ : pi1C = pr1 (pr2 (pr1 (has_prods C c_C d_C)))).
+
+             rewrite <-pi1R.
+              pose (pi2^R := pr2 (pr2 (pr1 prods_C))).
+             unfold has_prods.
+             simpl.
+
+             simpl.
+             unfold inv_from_iso.
+             unfold invmap.
+            }
+
+
+
+          }
+
+
+        }
+      }
+      {
+        admit.
+      }
+    }
+    {admit.}
+    all: admit.
+  Admitted.
